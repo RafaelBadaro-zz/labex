974320c2f3080b16ec7fa908cf4a91b2f5d8a226
Rafael Badaró <rafabadaro.15@gmail.com>
2020-03-01 12:42:12 -0300

    Criando métodos para melhora a legibilidade do código

--- script.py
+++ script.py
@@ -2,10 +2,8 @@ import requests
 import time
 import csv
 
-headers = {"Authorization": "token "}
+headers = {"Authorization": "token ###"}
 
-endCursor = "null"  # Proxima pagina
-nodes = []  # Resultados da query
 
 
 def run_query(query):
@@ -21,57 +19,61 @@ def run_query(query):
         raise Exception("Query failed to run by returning code of {}. {}".format(
             request.status_code, query))
 
+#Minera os repositórios
+def mine(first, interval):
+  endCursor = "null"  # Proxima pagina
+  nodes = []  # Resultados da query
 
-for x in range(50):
+  for x in range(interval):
     # GraphQL query
     query = """
-  {
-  search(query: "stars:>100", type: REPOSITORY, first: 20, after:%s) {
-    pageInfo {
-      endCursor
-    }
-    nodes {
-      ... on Repository {
-        nameWithOwner
-        url
-        createdAt
-        updatedAt
-        pullRequests(states: MERGED) {
-          totalCount
-        }
-        releases {
-          totalCount
-        }
-        primaryLanguage{
-          name
-        }
-        numeroTotalIssues: issues {
-          totalCount
-        }
-        numeroTotalIssuesClosed: issues(states: CLOSED) {
-          totalCount
+    {
+    search(query: "stars:>100", type: REPOSITORY, first: %s, after:%s) {
+      pageInfo {
+        endCursor
+      }
+      nodes {
+        ... on Repository {
+          nameWithOwner
+          url
+          createdAt
+          updatedAt
+          pullRequests(states: MERGED) {
+            totalCount
+          }
+          releases {
+            totalCount
+          }
+          primaryLanguage{
+            name
+          }
+          numeroTotalIssues: issues {
+            totalCount
+          }
+          numeroTotalIssuesClosed: issues(states: CLOSED) {
+            totalCount
+          }
         }
       }
     }
-  }
-  }
-  """ % (endCursor)
+    }
+    """ % (first, endCursor)
 
-    # O resultado da query que contem a proxima pagina e os nodes
+      # O resultado da query que contem a proxima pagina e os nodes
     queryResult = run_query(query)
     querySize = len(queryResult['data']['search']['nodes'])
     for y in range(querySize):
-        # Salva os nodes no array de nodes
-        nodes.append(queryResult["data"]["search"]["nodes"][y])
-        # Pega o endCursor aka proxima pagina
-        endCursor = '"{}"'.format(
-            queryResult["data"]["search"]["pageInfo"]["endCursor"])
-
-
-# print(nodes)
+          # Salva os nodes no array de nodes
+          nodes.append(queryResult["data"]["search"]["nodes"][y])
+          # Pega o endCursor aka proxima pagina
+          endCursor = '"{}"'.format(
+              queryResult["data"]["search"]["pageInfo"]["endCursor"])
+  
+  return nodes
 
 # Escreve em um arquivo csv
-with open("/Users/Rafael/Desktop/labex/repos.csv", 'w') as new_file:
+def writeCsv(nodes):
+  with open("/Users/Rafael/Desktop/labex/repos.csv", 'w') as new_file:
 
     fnames = [
         'name_with_owner',
@@ -101,3 +103,7 @@ with open("/Users/Rafael/Desktop/labex/repos.csv", 'w') as new_file:
             })
 
     print('Arquivo csv gerado com sucesso!')
+
+
+nodes = mine(20, 50)
+writeCsv(nodes)
